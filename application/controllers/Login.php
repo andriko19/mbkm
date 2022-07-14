2a76702889d98e1332e3c0293c32a1ef6cbaf43e
andriko19 <andrik.suprayitno@gmail.com>
2022-07-14 15:33:45 +0700

    edit file config.php

---
+++ application/controllers/Login.php
@@ -0,0 +1,132 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	public function __construct()
+	{
+		parent:: __construct();
+		$this->load->library('form_validation');
+		// $this->load->model('users_model');
+	}
+
+	public function index()
+	{
+		// if ($this->session->userdata('username')) {
+		// 	redirect('templates/admin_login');
+		// }
+
+		$this->form_validation->set_rules('username', 'Username', 'required|trim', [
+			'required' => 'Tidak Boleh Kosong!'
+		]);
+		$this->form_validation->set_rules('password', 'Password', 'required|trim', [
+			'required' => 'Tidak Boleh Kosong!'
+		]);
+
+		if ($this->form_validation->run() == false) {
+			// $data['title'] = 'Login';
+			// $this->load->view('templates/auth-header', $data);
+			$this->load->view('templates/admin_login');
+			// $this->load->view('templates/auth-footer');
+		} else {
+			// apabila validasi alert-success
+			$this->_login();
+		}
+	}
+
+	private function _login()
+	{
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+
+		$user = $this->db->get_where('users', ['username' => $username])->row_array();
+		?>
+			<link rel="stylesheet" href="<?= base_url(); ?>assets/sweetalert2/sweetalert2.min.css">
+			<link rel="stylesheet" href="<?= base_url(); ?>assets/sweetalert2/animate.min.css">
+			<script src="<?= base_url(); ?>assets/Sweetalert2/Sweetalert2.min.js"></script>
+			<style type="text/css">
+				body {
+					font-family: "Helvetice Neve", Helvetice, Arial, sans-serif;
+					font-size : 1.124em;
+					font-weight: normal;
+				}
+			</style>
+			<body></body>
+		<?php
+		// jika usernya ada
+		if ($user) {
+			// jika passwornya benar
+			if (password_verify($password, $user['password'])) {
+				// jika benar
+				$data = [
+					'username' 	 		=> $user['username'],
+					'nama_depan' 		=> $user['nama_depan'],
+					'nama_belakang' => $user['nama_belakang'],
+					'role' 					=> $user['role']
+				];
+				$this->session->set_userdata($data);
+				if ($user['role'] == 'administrator') {
+					?>
+						<script type="text/javascript">
+							Swal.fire({
+				        icon: 'success',
+				        title: 'Success',
+				        text: 'Selamat Admin, Anda Berhasil Login!'
+				      }).then((result) => {
+				      	window.location='<?=site_url('admin')?>';
+				      }) 
+						</script>
+					<?php
+				} else {
+					?>
+						<script type="text/javascript">
+							Swal.fire({
+				        icon: 'success',
+				        title: 'Success',
+				        text: 'Selamat Admin BEM atau UKM, Anda Berhasil Login!'
+				      }).then((result) => {
+				      	window.location='<?=site_url('admin/dashboard_informasi')?>';
+				      }) 
+						</script>
+					<?php
+				}
+			} else {
+				?>
+					<script type="text/javascript">
+						Swal.fire({
+			        icon: 'error',
+			        title: 'Peringatan',
+			        text: 'Password Anda Salah!'
+			      }).then((result) => {
+			      	window.location='<?=site_url('home')?>';
+			      }) 
+					</script>
+				<?php
+			}
+		} else {
+			?>
+				<script type="text/javascript">
+					Swal.fire({
+		        icon: 'error',
+		        title: 'Peringatan',
+		        text: 'Username Belum Terdaftar!'
+		      }).then((result) => {
+		      	window.location='<?=site_url('home')?>';
+		      }) 
+				</script>
+			<?php
+		}
+	}
+
+	public function logout() 
+	{
+		$this->session->unset_userdata('username');
+		$this->session->unset_userdata('nama_depan');
+		$this->session->unset_userdata('nama_belakang');
+		$this->session->unset_userdata('role');
+
+		$this->session->set_flashdata('message', 'Anda Sudah Berhasil Logout.');
+		redirect('home');
+	}
+
+}
---
+++ application/views/home.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>SSO - UWP</title>
+  <link rel="shortcut icon" type="image/jpg" href="<?= base_url(); ?>assets/img/favicon.png" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="" />
+  <meta name="author" content="http://ict.uwp.ac.id/" />
+  <!-- css -->
+  <link href="<?= base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="<?= base_url(); ?>assets/css/fancybox/jquery.fancybox.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>assets/css/jcarousel.css" rel="stylesheet" />
+  <link href="<?= base_url(); ?>assets/css/flexslider.css" rel="stylesheet" />
+  <link href="<?= base_url(); ?>assets/js/owl-carousel/owl.carousel.css" rel="stylesheet">
+  <link href="<?= base_url(); ?>assets/css/style.css" rel="stylesheet" />
+  
+  <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+<div id="wrapper">
+	<!-- start header -->
+	<header>
+    <div class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?= base_url(); ?>home"><img src="<?= base_url(); ?>assets/img/sso-logo.png" alt="logo"/></a>
+        </div>
+          <div class="navbar-collapse collapse ">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="<?= base_url(); ?>home">Beranda</a></li> 
+              <li><a href="#callaction">Tentang sso</a></li>
+              <li><a href="#content">List Link</a></li>
+              <li><a href="#kontak">Kontak</a></li>
+            </ul>
+          </div>
+      </div>
+    </div>
+	</header>
+	<!-- end header -->
+
+	<section id="featured">
+    <!-- Slider -->
+    <div id="main-slider" class="flexslider">
+      <ul class="slides">
+        <li>
+          <img src="<?= base_url(); ?>assets/img/slides/gedung-a.jpg" alt="" />
+          <!-- <div class="flex-caption">
+            <div class="item_introtext"> 
+              <strong>Online Education</strong>
+              <p>The best educational template</p> 
+            </div>
+          </div> -->
+        </li>
+        <li>
+          <img src="<?= base_url(); ?>assets/img/slides/2.jpg" alt="" />
+          <!-- <div class="flex-caption">
+            <div class="item_introtext"> 
+              <strong>School Education</strong>
+              <p>Get all courses with on-line content</p> 
+            </div>
+          </div> -->
+        </li>
+        <li>
+          <img src="<?= base_url(); ?>assets/img/slides/3.jpg" alt="" />
+          <!-- <div class="flex-caption">
+            <div class="item_introtext"> 
+              <strong>Collage Education</strong>
+              <p>Awesome Template get it know</p> 
+            </div>
+          </div> -->
+        </li>
+      </ul>
+    </div>
+    <!-- end slider -->
+	</section>
+
+	<section class="callaction" id="callaction">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="aligncenter"><h1 class="aligncenter">Our Featured Courses</h1><span class="clear spacer_responsive_hide_mobile " style="height:13px;display:block;"></span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident, doloribus omnis minus temporibus perferendis nesciunt quam repellendus nulla nemo ipsum odit corrupti consequuntur possimus, vero mollitia velit ad consectetur. Alias, laborum excepturi nihil autem nemo numquam, ipsa architecto non, magni consequuntur quam.</div>
+        </div>
+      </div>
+    </div>
+	</section>
+
+	<section id="content">
+    <div class="container">
+      <div class="row">
+        <div class="skill-home"> 
+          <div class="skill-home-solid clearfix"> 
+            <div class="col-md-3 text-center">
+              <span class="icons c1"><i class="fa fa-trophy"></i></span>
+              <div class="box-area">
+              <h3>Web Development</h3> 
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident</p>
+              <div class="row">
+                <div class="col-md-6 icon-sso">
+                  <a href=""> <img src="<?= base_url(); ?>assets/img/icon-sso/1.png" alt="logo"/> </a>
+                  <a href=""> <img src="<?= base_url(); ?>assets/img/icon-sso/1.png" alt="logo"/> </a>
+                </div>
+                <div class="col-md-6 icon-sso">
+                  <a href=""> <img src="<?= base_url(); ?>assets/img/icon-sso/1.png" alt="logo"/> </a>
+                  <a href=""> <img src="<?= base_url(); ?>assets/img/icon-sso/1.png" alt="logo"/> </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3 text-center"> 
+            <span class="icons c2"><i class="fa fa-picture-o"></i></span> 
+            <div class="box-area">
+              <h3>UI Design</h3> 
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident</p>
+            </div>
+          </div>
+          <div class="col-md-3 text-center"> 
+            <span class="icons c3"><i class="fa fa-desktop"></i></span> 
+            <div class="box-area">
+              <h3>Interaction</h3> 
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident</p>
+            </div>
+          </div>
+          <div class="col-md-3 text-center"> 
+            <span class="icons c4"><i class="fa fa-globe"></i></span> 
+            <div class="box-area">
+              <h3>User Experiance</h3> 
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores quae porro consequatur aliquam, incidunt eius magni provident</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> 
+	</section>
+
+  <footer id="kontak">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3">
+          <div class="widget">
+            <h5 class="widgetheading">Our Contact</h5>
+            <address>
+            <strong>Abovecompany Inc</strong><br>
+            JC Main Road, Near Silnile tower<br>
+            Pin-21542 NewYork US.</address>
+            <p>
+              <i class="icon-phone"></i> (123) 456-789 - 1255-12584 <br>
+              <i class="icon-envelope-alt"></i> email@domainname.com
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="widget">
+            <h5 class="widgetheading">Quick Links</h5>
+            <ul class="link-list">
+              <li><a href="#">Latest Events</a></li>
+              <li><a href="#">Terms and conditions</a></li>
+              <li><a href="#">Privacy policy</a></li>
+              <li><a href="#">Career</a></li>
+              <li><a href="#">Contact us</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="widget">
+            <h5 class="widgetheading">Latest posts</h5>
+            <ul class="link-list">
+              <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+              <li><a href="#">Pellentesque et pulvinar enim. Quisque at tempor ligula</a></li>
+              <li><a href="#">Natus error sit voluptatem accusantium doloremque</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="widget">
+            <h5 class="widgetheading">Recent News</h5>
+            <ul class="link-list">
+              <li><a href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
+              <li><a href="#">Pellentesque et pulvinar enim. Quisque at tempor ligula</a></li>
+              <li><a href="#">Natus error sit voluptatem accusantium doloremque</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="sub-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="copyright">
+              <p>
+                <span>&copy; Above Site All right reserved. Template By </span><a href="http://webthemez.com" target="_blank">WebThemez</a>
+              </p>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <ul class="social-network">
+              <li><a href="#" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#" data-placement="top" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+              <li><a href="#" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a></li>
+              <li><a href="#" data-placement="top" title="Google plus"><i class="fa fa-google-plus"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</div>
+<a href="#" class="scrollup"><i class="fa fa-angle-up active"></i></a>
+<!-- javascript
+    ================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="<?= base_url(); ?>assets/js/jquery.js"></script>
+<script src="<?= base_url(); ?>assets/js/jquery.easing.1.3.js"></script>
+<script src="<?= base_url(); ?>assets/js/bootstrap.min.js"></script>
+<script src="<?= base_url(); ?>assets/js/jquery.fancybox.pack.js"></script>
+<script src="<?= base_url(); ?>assets/js/jquery.fancybox-media.js"></script> 
+<script src="<?= base_url(); ?>assets/js/portfolio/jquery.quicksand.js"></script>
+<script src="<?= base_url(); ?>assets/js/portfolio/setting.js"></script>
+<script src="<?= base_url(); ?>assets/js/jquery.flexslider.js"></script>
+<script src="<?= base_url(); ?>assets/js/animate.js"></script>
+<script src="<?= base_url(); ?>assets/js/custom.js"></script>
+<script src="<?= base_url(); ?>assets/js/owl-carousel/owl.carousel.js"></script>
+</body>
+</html>
